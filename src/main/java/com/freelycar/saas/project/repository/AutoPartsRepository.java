3468910e34a17f889d3f04732f98c959204bbcd9
TangWei <toby911115@gmail.com>
2018-12-05 17:37:51 +0800

    1.新增AutoPartsRepository.java

---
+++ src/main/java/com/freelycar/saas/project/repository/AutoPartsRepository.java
@@ -0,0 +1,12 @@
+package com.freelycar.saas.project.repository;
+
+import com.freelycar.saas.project.entity.AutoParts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author tangwei - Toby
+ * @date 2018/12/5
+ * @email toby911115@gmail.com
+ */
+public interface AutoPartsRepository extends JpaRepository<AutoParts,String> {
+}
