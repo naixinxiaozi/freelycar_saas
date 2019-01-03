22f11d81a34e706c46453959ec66b35be43bbc0a
TangWei <toby911115@gmail.com>
2019-01-03 15:24:21 +0800

    1.项目列表查询写法有问题，无法支持项目类别不选择的情况。增加且修改了部分方法，使满足需求

--- src/main/java/com/freelycar/saas/project/service/ProjectService.java
+++ src/main/java/com/freelycar/saas/project/service/ProjectService.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -100,9 +101,14 @@ public class ProjectService {
      * @param pageSize
      * @return
      */
-    public PaginationRJO list(String storeId, Integer currentPage, Integer pageSize,String name,String projectTypeId) {
-        logger.debug("storeId:" + storeId);
-        Page<Project> projectPage = projectRepository.findAllByDelStatusAndStoreIdAndNameContainingAndProjectTypeId(DelStatus.EFFECTIVE.isValue(), storeId,name,projectTypeId, PageableTools.basicPage(currentPage, pageSize));
+    public PaginationRJO list(String storeId, Integer currentPage, Integer pageSize, String name, String projectTypeId) {
+        Pageable pageable = PageableTools.basicPage(currentPage, pageSize);
+        Page<Project> projectPage;
+        if (StringUtils.isEmpty(projectTypeId)) {
+            projectPage = projectRepository.findAllByDelStatusAndStoreIdAndNameContaining(DelStatus.EFFECTIVE.isValue(), storeId, name, pageable);
+        } else {
+            projectPage = projectRepository.findAllByDelStatusAndStoreIdAndNameContainingAndProjectTypeId(DelStatus.EFFECTIVE.isValue(), storeId, name, projectTypeId, pageable);
+        }
         return PaginationRJO.of(projectPage);
     }
 
