c81af0697b7faf78c225d4245fb4415a748b361a
tangtang233 <toby911115@163.com>
2019-02-01 11:26:46 +0800

    1.结算中心：获取用户所有会员卡

--- src/main/java/com/freelycar/saas/project/controller/CardController.java
+++ src/main/java/com/freelycar/saas/project/controller/CardController.java
@@ -4,6 +4,8 @@ import com.freelycar.saas.aop.LoggerManage;
 import com.freelycar.saas.basic.wrapper.ResultJsonObject;
 import com.freelycar.saas.project.entity.Card;
 import com.freelycar.saas.project.service.CardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +16,17 @@ import org.springframework.web.bind.annotation.*;
  * @date 2018-12-27
  * @email toby911115@gmail.com
  */
+@Api(value = "会员卡接口", tags = "会员卡服务接口")
 @RestController
 @RequestMapping("/card")
 public class CardController {
     private Logger logger = LoggerFactory.getLogger(CardController.class);
     @Autowired
     private CardService cardService;
-    private String errorMsg;
 
+    @ApiOperation(value = "开卡", produces = "application/json")
     @PostMapping("/handleCard")
+    @LoggerManage(description = "调用方法：开卡")
     public ResultJsonObject handleCard(@RequestBody Card card) {
 
         return cardService.handleCard(card);
@@ -33,14 +37,20 @@ public class CardController {
      * @param id
      * @return
      */
-    @GetMapping(value = "/detail")
+    @ApiOperation(value = "获取会员卡详情", produces = "application/json")
+    @GetMapping("/detail")
     @LoggerManage(description = "调用方法：获取会员卡详情")
     public ResultJsonObject detail(@RequestParam String id) {
-        if (null == id) {
-            errorMsg = "接收到的参数：id为NULL";
-            logger.error(errorMsg);
-            return ResultJsonObject.getErrorResult(null, errorMsg);
-        }
         return cardService.getDetail(id);
+    }
+
+    @ApiOperation(value = "获取客户的所有会员卡", produces = "application/json")
+    @GetMapping("/getMyCards")
+    @LoggerManage(description = "调用方法：获取客户的所有会员卡")
+    public ResultJsonObject getMyCards(
+            @RequestParam String id,
+            @RequestParam String storeId
+    ) {
+        return cardService.getMyCards(id, storeId);
     }
 }
