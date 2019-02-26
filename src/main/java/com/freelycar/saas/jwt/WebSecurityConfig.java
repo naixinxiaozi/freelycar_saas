be3220e328203187d54ae6a4354da968c7434bfb
tangtang233 <toby911115@163.com>
2019-02-26 10:02:45 +0800

    1.技师登录接口添加到白名单

--- src/main/java/com/freelycar/saas/jwt/WebSecurityConfig.java
+++ src/main/java/com/freelycar/saas/jwt/WebSecurityConfig.java
@@ -43,6 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/wechat/config/**").permitAll()
                 //放行微信登录相关接口
                 .antMatchers("/wechat/login/**").permitAll()
+                .antMatchers("/wechat/staff/login").permitAll()
                 //测试请求mobile
                 .antMatchers(HttpMethod.GET, "/mobile/**").hasRole("ADMIN")
                 // 添加权限检测
