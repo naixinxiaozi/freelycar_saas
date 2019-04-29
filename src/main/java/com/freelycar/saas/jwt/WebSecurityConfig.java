d3545ddf8e264e333130ad5dc80ba7ea976486eb
tangtang233 <toby911115@163.com>
2019-04-29 10:14:31 +0800

    1.放行druid监控的页面

--- src/main/java/com/freelycar/saas/jwt/WebSecurityConfig.java
+++ src/main/java/com/freelycar/saas/jwt/WebSecurityConfig.java
@@ -37,6 +37,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/v2/**").permitAll()
                 .antMatchers("/swagger-resources/**").permitAll()
                 .antMatchers("/swagger-ui.html/**").permitAll()
+                //放行Druid监控页面
+                .antMatchers("/druid/**").permitAll()
                 //放行与iotcloudcn通信的接口
                 .antMatchers("/iot/**").permitAll()
                 //放行微信公众号SDK接口
