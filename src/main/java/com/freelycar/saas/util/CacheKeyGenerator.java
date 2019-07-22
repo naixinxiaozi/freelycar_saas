4b17cbc2c2d79e7aec3f5febecb71aaba7c1f7e4
tangtang233 <toby911115@163.com>
2019-07-22 14:14:39 +0800

    test(Cache): 缓存keyGenerator测试

--- src/main/java/com/freelycar/saas/util/CacheKeyGenerator.java
+++ src/main/java/com/freelycar/saas/util/CacheKeyGenerator.java
@@ -56,6 +56,7 @@ public class CacheKeyGenerator implements KeyGenerator {
                 return targetName + DOT + methodName;
             }
         }
+        logger.debug("缓存key：" + s);
         return s;
     }
 
