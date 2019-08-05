eac8b178e220c7da4b4231a60a487f1a3297fc7b
tangtang233 <toby911115@163.com>
2019-08-05 12:05:27 +0800

    :wrench: Changing configuration files. - jwt缓存时间改为90天（试运行）

--- src/main/java/com/freelycar/saas/jwt/TokenAuthenticationUtil.java
+++ src/main/java/com/freelycar/saas/jwt/TokenAuthenticationUtil.java
@@ -28,7 +28,7 @@ public class TokenAuthenticationUtil {
 
     private static Logger logger = LoggerFactory.getLogger(TokenAuthenticationUtil.class);
 
-    private static final long EXPIRATIONTIME = 432_000_000;     // 5天
+    private static final long EXPIRATIONTIME = 7776000000L;     // 4周
     //        private static final long EXPIRATIONTIME = 300_000;     // 5分钟测试用
     private static final String SECRET = "FreelyC@r";            // JWT密码
     private static final String TOKEN_PREFIX = "Bearer";        // Token前缀
